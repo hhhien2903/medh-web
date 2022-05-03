1bace51a883ac618fb93caca4c1fdaa0f8091f73
mrbin2903 <hhhien2903@gmail.com>
2022-05-03 16:42:37 +0700

    dashboard

--- src/pages/expert/ExpertDashboard/ExpertDashboard.jsx
+++ src/pages/expert/ExpertDashboard/ExpertDashboard.jsx
@@ -1,7 +1,116 @@
+import { Col, Row } from 'antd';
+import { FaUserFriends, FaDisease, FaMicrochip, FaUserMd } from 'react-icons/fa';
+import { RiHospitalFill } from 'react-icons/ri';
+import { BsFileEarmarkMedicalFill } from 'react-icons/bs';
+import doctorAPI from '../../../api/doctorAPI';
+import CardStatistic from '../../../components/shared/CardStatistic/CardStatistic';
+import useLoadingSkeleton from '../../../components/shared/LoadingSkeleton/useLoadingSkeleton';
+import { useEffect, useState } from 'react';
+import patientAPI from '../../../api/patientAPI';
+import deviceAPI from '../../../api/deviceAPI';
+import diseaseAPI from '../../../api/diseaseAPI';
+import ruleConditionAPI from '../../../api/ruleConditionAPI';
+import hospitalAPI from '../../../api/hospitalAPI';
 const ExpertDashboard = () => {
+  const { renderLoadingSkeleton, setIsLoadingSkeleton, isLoadingSkeleton } = useLoadingSkeleton();
+
+  const [statisticSource, setStatisticSource] = useState([]);
+
+  const getAllInfoStatistic = async () => {
+    try {
+      setIsLoadingSkeleton(true);
+      const doctorSourceResult = await doctorAPI.getAllDoctors();
+      const patientSourceResult = await patientAPI.getAllPatients();
+      const deviceSourceResult = await deviceAPI.getAllDevices();
+      const diseaseSourceResult = await diseaseAPI.getAllDiseases();
+      const ruleSourceResult = await ruleConditionAPI.getAllRuleConditions();
+      const hospitalSourceResult = await hospitalAPI.getAllHospital();
+      setStatisticSource({
+        doctor: doctorSourceResult.length,
+        patient: patientSourceResult.length,
+        device: deviceSourceResult.length,
+        disease: diseaseSourceResult.length,
+        rule: ruleSourceResult.length,
+        hospital: hospitalSourceResult.length,
+      });
+      setIsLoadingSkeleton(false);
+    } catch (error) {
+      setIsLoadingSkeleton(true);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllInfoStatistic();
+  }, []);
+
   return (
-    <div style={{ padding: 20 }}>
-      <h1>ExpertDashboard</h1>
+    <div style={{ padding: 40 }}>
+      {isLoadingSkeleton ? (
+        renderLoadingSkeleton
+      ) : (
+        <Row gutter={[20, 20]}>
+          <Col span={6}>
+            <CardStatistic
+              number={statisticSource?.doctor}
+              type={'Bác Sĩ'}
+              icon={<FaUserMd opacity={0.5} size={70} />}
+              iconBottom="-10px"
+              iconRight="-10px"
+            />
+          </Col>
+          <Col span={6}>
+            <CardStatistic
+              type={'Bệnh Nhân'}
+              number={statisticSource?.patient}
+              icon={<FaUserFriends opacity={0.5} size={85} />}
+              iconBottom="-20px"
+              iconRight="-15px"
+              cardBackground="linear-gradient(to right, #fdc830, #f37335)"
+            />
+          </Col>
+          <Col span={6}>
+            <CardStatistic
+              type={'Thiết Bị'}
+              number={statisticSource?.device}
+              icon={<FaMicrochip opacity={0.5} size={70} />}
+              iconBottom="-15px"
+              iconRight="-15px"
+              cardBackground="linear-gradient(to left, #348f50, #56b4d3)"
+            />
+          </Col>
+          <Col span={6}>
+            <CardStatistic
+              type={'Mầm Bệnh'}
+              number={statisticSource?.disease}
+              icon={<FaDisease opacity={0.5} size={85} />}
+              iconBottom="-20px"
+              iconRight="-15px"
+              cardBackground="linear-gradient(to right, #cc2b5e, #753a88)"
+            />
+          </Col>
+          <Col span={6}>
+            <CardStatistic
+              type={'Tập Luật Y Tế'}
+              number={statisticSource?.rule}
+              icon={<BsFileEarmarkMedicalFill opacity={0.5} size={75} />}
+              iconBottom="-20px"
+              iconRight="-15px"
+              cardBackground="linear-gradient(to left, #203a43, #2c5364)"
+            />
+          </Col>
+          <Col span={6}>
+            <CardStatistic
+              type={'Bệnh Viện'}
+              number={statisticSource?.hospital}
+              icon={<RiHospitalFill opacity={0.5} size={70} />}
+              iconBottom="-10px"
+              iconRight="-10px"
+              cardBackground="linear-gradient(to left, #4e54c8, #8f94fb)"
+            />
+          </Col>
+        </Row>
+      )}
     </div>
   );
 };
